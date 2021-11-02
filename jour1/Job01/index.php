a5b7f404302ab5e66a44a757d79407db972b55c4
loic-bodjolle <92152133+loic-bodjolle@users.noreply.github.com>
2021-11-02 12:01:14 +0100

    jour1 job 01

---
+++ jour1/Job01/index.php
@@ -0,0 +1,22 @@
+<?php  
+   
+   $str = " La Plateforme";
+   $str2 = "vive";
+   $str3 = "!";
+   echo $str2 . $str . $str3 ;
+
+   echo "<br>";
+  
+
+   $val = 10;
+   echo $val ;
+
+   echo "<br>";
+
+
+   $myBool = 'true false';
+   echo $myBool ;
+
+
+
+?>
