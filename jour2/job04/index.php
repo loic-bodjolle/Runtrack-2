1eb7489369fd403000c3577b687e2b60513b67c5
loic-bodjolle <92152133+loic-bodjolle@users.noreply.github.com>
2021-11-03 17:58:10 +0100

    jour 2 job04

---
+++ jour2/job04/index.php
@@ -0,0 +1,31 @@
+<?php
+
+$x = 0;
+
+while ($x <= 100) {
+    if (! ($x % 3) && ! ($x % 5 )) {
+        echo "fizzbuzz <br>";
+    }
+
+    elseif (!($x %  5 )) {
+        echo "buzz<br>";
+    }
+
+    elseif (!($x % 3)) {
+        echo " fizz<br>";
+    }
+
+    else {
+        echo"$x<br>";
+    }
+
+
+
+    $x = $x +1 ;
+}
+
+
+
+
+
+?>
