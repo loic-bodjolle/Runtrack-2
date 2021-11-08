35b8640789fc6d557ccfb8231db6b492b954613a
loic-bodjolle <92152133+loic-bodjolle@users.noreply.github.com>
2021-11-08 11:46:55 +0100

    jour5 job02

---
+++ jour5/job02/index.php
@@ -0,0 +1,23 @@
+<?php
+
+
+
+
+
+function bonjour($jour ){
+
+    if ($jour == true){
+    echo "Bonjour <br>";
+    }
+    else {
+        echo 'Bonsoir' ;
+    }
+}
+
+
+
+
+bonjour(false) ;
+bonjour(true)
+
+?>
